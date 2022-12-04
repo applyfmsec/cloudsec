d20bb88b19b59c067d90b74dae6f96a4e5401773
Joe Stubbs <joestubbs5@gmail.com>
2022-12-04 11:33:55 -0600

    Organize 'main program' and test code into functions so that the module can be easily imported, when being used as an external lib.

--- connectors/tapisfiles.py
+++ connectors/tapisfiles.py
@@ -12,43 +12,57 @@ from tapipy.tapis import Tapis
 # Add the cloudsec package directory to the python path so that 
 # the tests can run easily from within the tests Docker container
 sys.path.append('/home/cloudsec/cloudsec')
-print(f"Python path: {sys.path}")
 
-from core import Policy
-from cloud import tapis_files_policy_type, principal, tapis_file_perm
+from core import Policy, PolicyEquivalenceChecker
+from cloud import tapis_files_policy_type
 
 DEV_TENANT_USERS = [f"testuser{i}" for i in range(1,21)]
 
-base_url = os.environ.get('TAPIS_BASE_URL', "https://dev.develop.tapis.io")
-jwt = os.environ.get("TAPIS_JWT")
-if not jwt:
-    raise Exception("Setting the TAPIS_JWT environment variable is required.")
 
-t = Tapis(base_url=base_url, access_token=jwt)
-# make sure the credential is valid:
-try:
-    t.systems.getSystems()
-except Exception as e:
-    raise Exception(f"Could not verify credential; is the token valid? Got exception: {e}")
-
-# Tenant and Users
-# A valid JSON object set in the TAPIS_INPUT variable that includes a `users` object (array of strings)
-# and a `tenant` object (string) is required.
-tapis_input_raw = os.environ.get("TAPIS_INPUT")
-try:
-    tapis_input = json.loads(tapis_input_raw)
-    users = tapis_input['users']
-    tenant_id = tapis_input['tenant_id']
-except Exception as e:
-    raise Exception(f"Unable to parse TAPIS_INPUT variable; e: {e}")
-
-assert type(users) == list
-assert type(tenant_id) == str
-
-
-def get_files_perms_for_username(username, tenant_id):
+def get_tapis_client():
     """
-    Retrieve the files permissions for a specific username and tenan.
+    Get a configured Tapis client from the input environment variables.
+    """
+
+    base_url = os.environ.get('TAPIS_BASE_URL', "https://dev.develop.tapis.io")
+    jwt = os.environ.get("TAPIS_JWT")
+    if not jwt:
+        raise Exception("Setting the TAPIS_JWT environment variable is required.")
+
+    t = Tapis(base_url=base_url, access_token=jwt)
+    # make sure the credential is valid:
+    try:
+        t.systems.getSystems()
+    except Exception as e:
+        raise Exception(f"Could not verify credential; is the token valid? Got exception: {e}")
+
+    return t
+
+
+def get_input():
+    """
+    Parse the input environment variable, TAPIS_INPUT, into `users` and `tenant_id`.
+    """
+    # Tenant and Users
+    # A valid JSON object set in the TAPIS_INPUT variable that includes a `users` object (array of strings)
+    # and a `tenant` object (string) is required.
+    tapis_input_raw = os.environ.get("TAPIS_INPUT")
+    try:
+        tapis_input = json.loads(tapis_input_raw)
+        users = tapis_input['users']
+        tenant_id = tapis_input['tenant_id']
+    except Exception as e:
+        raise Exception(f"Unable to parse TAPIS_INPUT variable; e: {e}")
+
+    assert type(users) == list
+    assert type(tenant_id) == str
+    return users, tenant_id
+
+
+def get_files_perms_for_username(username, tenant_id, t):
+    """
+    Retrieve the files permissions for a specific username and tenant.
+    Requires a valid Tapis client, `t`, authenticated with some user for the tenant.
     """
     try:
         result = t.sk.getUserPerms(user=username, tenant=tenant_id)
@@ -78,24 +92,30 @@ def convert_files_perm_to_policy(username: str, tenant: str, perm: str) -> Polic
         perm_path = parts[4]
     p = Policy(policy_type=tapis_files_policy_type, 
                principal=(tenant, username), 
-               file_perm=(perm_tenant_id, perm_system_id, perm_path),
+               file_perm=(perm_tenant_id, perm_system_id, perm_level, perm_path),
                decision="allow")
     return p
 
 
-def main():
+def get_permissions_for_users(users, tenant_id, tapis_client):
     perms = {}
-    policies = []
-    # check if there is a single user, "*" -- we allow this for the dev tenant.
-    global users
-    global tenant_id
-    if len(users) == 1 and users[0] == "*" and tenant_id == "dev":
-        users = DEV_TENANT_USERS
     for user in users:
-        perms[user] = get_files_perms_for_username(user, tenant_id)
+        perms[user] = get_files_perms_for_username(user, tenant_id, tapis_client)
         print(f"Got {len(perms[user])} total permissions for {user} in tenant {tenant_id}.")
+    return perms
+
+
+def get_files_policies_for_perms(perms_dict, tenant_id):
+    """
+    This function converts permissions to policies.
+    It accepts a permissions dictionary, `perms_dict`, whose keys are the usernames associated with a list 
+    of permissions and whose values are Python lists of strings which are the permissions strings. 
+    
+    The result returned isa flattened list of policies for all users.
+    """
+    policies = []
     current_policy_count = 0
-    for user, ps in perms.items():
+    for user, ps in perms_dict.items():
         for perm_spec in ps:
             # some of the permission records will be for different specs; e.g., apps, etc.
             if perm_spec.startswith("files"):
@@ -103,6 +123,43 @@ def main():
         policies_for_user = len(policies) - current_policy_count
         print(f"Generated {policies_for_user} policies for user {user}.")
         current_policy_count = len(policies)
+    return policies
+
+
+def test():
+    """
+    An example test function which can also be called from the Python shell to get some example objects.
+    """
+    tapis_client = get_tapis_client()
+    tenant_id = "dev"
+    users = ["*"]
+    perms = get_permissions_for_users(users, tenant_id, tapis_client)
+    policies = get_files_policies_for_perms(perms)
+    # another policy that we want to use to check the equivalence of
+    q = Policy(policy_type=tapis_files_policy_type, 
+            principal=('dev', 'testuser2'), 
+            file_perm=('dev', 'AFkAxgqfHa', '/foo'), 
+            decision='allow')
+
+    ch = PolicyEquivalenceChecker(policy_type=tapis_files_policy_type, policy_set_p=policies, policy_set_q=[q])
+    ch.encode()
+    # Q has just the one allow policy which is a special case of one of the P allow policies, so Q=>P is clear:
+    assert ch.q_implies_p()
+    # return all the objects in case being called from the Python shell
+    return tapis_client, tenant_id, users, perms, policies, q, ch
+
+
+def main():
+    perms = {}
+    tapis_client = get_tapis_client()
+    users, tenant_id = get_input()
+    # check if there is a single user, "*" -- we allow this for the dev tenant.
+    if len(users) == 1 and users[0] == "*" and tenant_id == "dev":
+        users = DEV_TENANT_USERS
+    perms = get_permissions_for_users(users, tenant_id, tapis_client)
+    for user in users:
+        print(f"Got {len(perms[user])} total permissions for {user} in tenant {tenant_id}.")
+    policies = get_files_policies_for_perms(perms, tenant_id)
     print(f"Total policies generated: {len(policies)}")
         
 if __name__ == "__main__":
